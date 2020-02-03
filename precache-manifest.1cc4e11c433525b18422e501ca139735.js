18d655fbb862a8ba52e6fefbe489c985a54e19c1
fitkovich1 <fitkovich1@gmail.com>
2020-02-03 20:35:57 +0300

    Updates

---
+++ precache-manifest.1cc4e11c433525b18422e501ca139735.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaead74ed0a3411d24733b418f581e46",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "ee567acad9229e25b9d9",
+    "url": "/tic-tac-toe/static/css/main.bc249a96.chunk.css"
+  },
+  {
+    "revision": "ee3134cc5ee5bc24d0b0",
+    "url": "/tic-tac-toe/static/js/2.649d9e7d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tic-tac-toe/static/js/2.649d9e7d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ee567acad9229e25b9d9",
+    "url": "/tic-tac-toe/static/js/main.72b2b7ba.chunk.js"
+  },
+  {
+    "revision": "fdbd6fae367d142c593b",
+    "url": "/tic-tac-toe/static/js/runtime-main.df4931a8.js"
+  }
+]);
